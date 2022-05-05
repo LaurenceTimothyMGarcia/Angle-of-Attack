4d93bf6c761fd4bcfd2f9fb3e19e395e7fcdebca
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-05-04 23:37:25 -0700

    added main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Multiplayer()
+    {
+        SceneManager.LoadScene("MultiplayerMode");
+    }
+
+    public void Showcase()
+    {
+        SceneManager.LoadScene("ShowcaseMode");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
